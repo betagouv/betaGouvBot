b2346f4546ad294618ee78cb2494005d9af211e9
Laurent Bossavit <laurent.git@bossavit.com>
2016-10-12 16:20:03 +0200

    Simplify schedule data structure, just find ends at +1 and +10 days

--- notifier.rb
+++ notifier.rb
@@ -2,13 +2,11 @@ require 'date'
 require 'active_support/core_ext/hash/indifferent_access'
 
 def notifications members, date
+	result = {}
 	by_date = members.map(&:with_indifferent_access).group_by {|item| item[:end]}
-	by_date = by_date.select {|itsdate,_| !itsdate.to_s.empty? && (Date.parse itsdate) > date}
-	by_date.flat_map do |date,items|
-		date = Date.parse date
-		soon = (date - 10).iso8601
-		late = (date - 1).iso8601
-		who = items.map {|one| one[:fullname]}
-		[{when:soon, who: who},{when:late, who: who}]
-	end
+	tomorrow = (date + 1).iso8601
+	soon = (date + 10).iso8601
+	result[:tomorrow] = by_date[tomorrow].map {|one| one[:fullname]} if by_date[tomorrow]
+	result[:soon] = by_date[soon].map {|one| one[:fullname]} if by_date[soon]
+	result
 end
