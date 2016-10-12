75b2ec23d244f26924f16c0e09ee8439672637ff
Laurent Bossavit <laurent.git@bossavit.com>
2016-10-12 14:10:51 +0200

    Test for n items

--- notifier.rb
+++ notifier.rb
@@ -1,10 +1,10 @@
 require 'date'
 
 def notifications members, date
-	return [] if members.empty?
-	item = members.first
-	date = Date.parse item[:end]
-	soon = (date - 10).iso8601
-	late = (date - 1).iso8601
-	[{when:soon, who:[item[:id]]},{when:late, who:[item[:id]]}]
+	members.flat_map do |item|
+		date = Date.parse item[:end]
+		soon = (date - 10).iso8601
+		late = (date - 1).iso8601
+		[{when:soon, who:[item[:id]]},{when:late, who:[item[:id]]}]
+	end
 end
--- spec/notifications_spec.rb
+++ spec/notifications_spec.rb
@@ -20,5 +20,13 @@ RSpec.describe "generating notifications:" do
 				end
 			end
 
+			context "when two members have a future end date" do
+				it "generates four notifications" do
+					members = [{id: "author/lbo",end: "2016-12-01"},{id: "author/you",end: "2016-12-02"}]
+					date = Date.parse('2016-12-01')
+					expect(notifications(members,date)).to eq [{when:"2016-11-21",who:["author/lbo"]},{when:"2016-11-30",who:["author/lbo"]},
+					  {when:"2016-11-22",who:["author/you"]},{when:"2016-12-01",who:["author/you"]}]
+				end
+			end
 	end
 end
