cb1fce45befc7d71e00468a7ba71959068178deb
Laurent Bossavit <laurent.git@bossavit.com>
2016-10-12 16:22:03 +0200

    Send reminders via SendGrid

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+gem 'rspec', group: 'test'
+
+gem 'httparty'
+gem 'activesupport'
+gem 'sendgrid-ruby'
--- webhook.rb
+++ webhook.rb
@@ -1,8 +1,36 @@
 require 'sinatra'
-require 'json'
+require 'httparty'
+require 'date'
+require 'sendgrid-ruby'
 
-post '/payload' do
-  push = JSON.parse(request.body.read)
-  puts "I got some JSON: #{push.inspect}"
+require './notifier'
+
+include SendGrid
+
+helpers do
+	def send_reminded members, when		
+		from = Email.new(email: 'betaGouvBot@beta.gouv.fr')
+		subject = 'Rappel: arrivée à échéance de contrats!'
+		to = Email.new(email: 'contact@beta.gouv.fr')
+		content = Content.new(type: 'text/plain', value: "Les contrats de #{members.join(',')} arrivent à échéance #{when}\n\n-- BetaGouvBot")
+		mail = Mail.new(from, subject, to, content)
+
+		sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+		response = sg.client.mail._('send').post(request_body: mail.to_json)
+		# Log results for debugging
+		puts response.status_code
+		puts response.body
+		puts response.headers
+	end
 end
 
+post '/payload' do
+	# Read beta.gouv.fr members API
+	response = HTTParty.get('https://beta.gouv.fr/api/v1/authors.json')
+	# Parse into a schedule of notifications
+	today = Date.today
+	members = response.parsed_response
+	schedule = notifications(members,today)
+	send_reminder schedule[:tomorrow], "demain" if schedule[:tomorrow]
+	send_reminder schedule[:soon], "dans 10 jours" if schedule[:soon]
+end
