1c1c6439cffe537700077c4216ba9145c6755e7c
Laurent Bossavit <laurent.git@bossavit.com>
2016-10-12 14:17:15 +0200

    Use full name instead of id

--- notifier.rb
+++ notifier.rb
@@ -6,7 +6,7 @@ def notifications members, date
 		date = Date.parse date
 		soon = (date - 10).iso8601
 		late = (date - 1).iso8601
-		who = items.map {|one| one[:id]}
+		who = items.map {|one| one[:fullname]}
 		[{when:soon, who: who},{when:late, who: who}]
 	end
 end
--- spec/notifications_spec.rb
+++ spec/notifications_spec.rb
@@ -14,27 +14,27 @@ RSpec.describe "generating notifications:" do
 
 			context "when one member has a future end date" do
 				it "generates two notifications" do
-					members = [{id: "author/lbo",end: "2016-12-01"}]
+					members = [{fullname: "lbo",end: "2016-12-01"}]
 					date = Date.parse('2016-12-01')
-					expect(notifications(members,date)).to eq [{when:"2016-11-21",who:["author/lbo"]},{when:"2016-11-30",who:["author/lbo"]}]
+					expect(notifications(members,date)).to eq [{when:"2016-11-21",who:["lbo"]},{when:"2016-11-30",who:["lbo"]}]
 				end
 			end
 
 			context "when two members have a future end date" do
 				it "generates four notifications" do
-					members = [{id: "author/lbo",end: "2016-12-01"},{id: "author/you",end: "2016-12-02"}]
+					members = [{fullname: "lbo",end: "2016-12-01"},{fullname: "you",end: "2016-12-02"}]
 					date = Date.parse('2016-12-01')
-					expect(notifications(members,date)).to eq [{when:"2016-11-21",who:["author/lbo"]},{when:"2016-11-30",who:["author/lbo"]},
-					  {when:"2016-11-22",who:["author/you"]},{when:"2016-12-01",who:["author/you"]}]
+					expect(notifications(members,date)).to eq [{when:"2016-11-21",who:["lbo"]},{when:"2016-11-30",who:["lbo"]},
+					  {when:"2016-11-22",who:["you"]},{when:"2016-12-01",who:["you"]}]
 				end
 			end
 
 			context "when two members have the same end date" do
 				it "generates two notifications" do
-					members = [{id: "author/lbo",end: "2016-12-01"},{id: "author/you",end: "2016-12-01"}]
+					members = [{fullname: "lbo",end: "2016-12-01"},{fullname: "you",end: "2016-12-01"}]
 					date = Date.parse('2016-12-01')
-					expect(notifications(members,date)).to eq [{when:"2016-11-21",who:["author/lbo","author/you"]},
-					  {when:"2016-11-30",who:["author/lbo","author/you"]}]
+					expect(notifications(members,date)).to eq [{when:"2016-11-21",who:["lbo","you"]},
+					  {when:"2016-11-30",who:["lbo","you"]}]
 				end
 			end
 	end
