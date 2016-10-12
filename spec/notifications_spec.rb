cddc41c5ec99ae6c0cbd08989391ed1cd7ee61dd
Laurent Bossavit <laurent.git@bossavit.com>
2016-10-12 14:26:47 +0200

    Fix fixture dates, filter out dates in past

--- spec/notifications_spec.rb
+++ spec/notifications_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe "generating notifications:" do
 			context "when one member has a future end date" do
 				it "generates two notifications" do
 					members = [{fullname: "lbo",end: "2016-12-01"}]
-					date = Date.parse('2016-12-01')
+					date = Date.parse('2016-10-12')
 					expect(notifications(members,date)).to eq [{when:"2016-11-21",who:["lbo"]},{when:"2016-11-30",who:["lbo"]}]
 				end
 			end
@@ -23,7 +23,7 @@ RSpec.describe "generating notifications:" do
 			context "when two members have a future end date" do
 				it "generates four notifications" do
 					members = [{fullname: "lbo",end: "2016-12-01"},{fullname: "you",end: "2016-12-02"}]
-					date = Date.parse('2016-12-01')
+					date = Date.parse('2016-10-12')
 					expect(notifications(members,date)).to eq [{when:"2016-11-21",who:["lbo"]},{when:"2016-11-30",who:["lbo"]},
 					  {when:"2016-11-22",who:["you"]},{when:"2016-12-01",who:["you"]}]
 				end
@@ -32,7 +32,7 @@ RSpec.describe "generating notifications:" do
 			context "when two members have the same end date" do
 				it "generates two notifications" do
 					members = [{fullname: "lbo",end: "2016-12-01"},{fullname: "you",end: "2016-12-01"}]
-					date = Date.parse('2016-12-01')
+					date = Date.parse('2016-10-12')
 					expect(notifications(members,date)).to eq [{when:"2016-11-21",who:["lbo","you"]},
 					  {when:"2016-11-30",who:["lbo","you"]}]
 				end
@@ -41,7 +41,15 @@ RSpec.describe "generating notifications:" do
 			context "when members have no end date" do
 				it "generates no notifications" do
 					members = [{fullname: "lbo",end: ""},{fullname: "you",end: ""}]
-					date = Date.parse('2016-12-01')
+					date = Date.parse('2016-10-12')
+					expect(notifications(members,date)).to eq []
+				end
+			end
+
+			context "when members have end date already past" do
+				it "generates no notifications" do
+					members = [{fullname: "lbo",end: "2016-09-30"},{fullname: "you",end: ""}]
+					date = Date.parse('2016-10-12')
 					expect(notifications(members,date)).to eq []
 				end
 			end
