e209b93adfe2a8a1bdaf09647ed8e84b2799734f
Laurent Bossavit <laurent.git@bossavit.com>
2016-11-15 13:56:11 +0100

    Fix typo

--- lib/betagouvbot/webhook.rb
+++ lib/betagouvbot/webhook.rb
@@ -6,14 +6,14 @@ require 'sendgrid-ruby'
 
 module BetaGouvBot
   class Webhook < Sinatra::Base
-    post '/payload' do
+    get '/payload' do
       # Read beta.gouv.fr members' API
       members  = HTTParty.get('https://beta.gouv.fr/api/v1/authors.json').parsed_response
 
       # Parse into a schedule of notifications
       schedule = Notifier.(members, Date.today)
 
-      # Send remainders (if any)
+      # Send reminders (if any)
       Mailer.(schedule[:tomorrow], 'demain') if schedule[:tomorrow]
       Mailer.(schedule[:soon], 'dans 10 jours') if schedule[:soon]
     end
