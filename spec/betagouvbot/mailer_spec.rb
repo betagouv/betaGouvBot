7682607758b8513b8e2601ae6cde421b534adf65
Laurent Bossavit <laurent.git@bossavit.com>
2017-01-10 11:37:07 +0100

    Add integration test for mailer

--- spec/betagouvbot/mailer_spec.rb
+++ spec/betagouvbot/mailer_spec.rb
@@ -2,10 +2,10 @@
 # frozen_string_literal: true
 
 RSpec.describe BetaGouvBot::Mailer do
-  let(:urgency) { :tomorrow }
-  let(:members) { %w(Ann Bob) }
+  describe 'content of emails' do
+    let(:members) { %w(Ann Bob) }
+    let(:urgency) { :tomorrow }
 
-  describe '.content' do
     subject(:content) { described_class.content(urgency, members).value }
 
     it { is_expected.to include('Les contrats de') }
@@ -14,4 +14,18 @@ RSpec.describe BetaGouvBot::Mailer do
     it { is_expected.to include('arrivent à échéance') }
     it { is_expected.to include('demain') }
   end
+
+ describe 'sending out emails' do
+   let(:authors)  { [id: 'ann', fullname: 'Ann', end: (Date.today+1).iso8601] }
+   let(:schedule) { BetaGouvBot::Anticipator.(authors, Date.today) }
+   let(:client)   { instance_spy('client') }
+
+   before { allow(described_class).to receive(:client) { client } }
+
+   it 'works' do
+     described_class.(schedule)
+     expect(client).to have_received(:post)
+   end
+  end
+
 end
