7e1a3a7ba0fe97d292e3c444f24df9f56651979c
Laurent Bossavit <laurent.git@bossavit.com>
2017-01-15 17:12:14 +0100

    Actually sort members and alumni

--- lib/betagouvbot/sortinghat.rb
+++ lib/betagouvbot/sortinghat.rb
@@ -1,6 +1,8 @@
 # encoding: utf-8
 # frozen_string_literal: true
 
+require 'active_support/core_ext/hash/indifferent_access'
+
 module BetaGouvBot
   module SortingHat
     module_function
@@ -9,8 +11,8 @@ module BetaGouvBot
       # Input: a date
       # Input: a list of members' arrivals and departures
       # @return [Hash<Array>] the list sorted into active members and alumni.
-      def call(members, date)
-        members, alumni = [[],[]]
+      def call(community, date)
+        members, alumni = community.map(&:with_indifferent_access).partition {|member| Date.iso8601(member[:end]) >= date}
         {members: members, alumni: alumni}
       end
 
--- spec/betagouvbot/sortinghat_spec.rb
+++ spec/betagouvbot/sortinghat_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe BetaGouvBot::SortingHat do
 
   let(:yesterday)     { today - 1 }
   let(:today)         { Date.today }
-  let(:tomorrow)      { today + 1 }
   let(:sorted)        { described_class.(members, today) }
 
   context 'when member list is empty' do
@@ -13,6 +12,16 @@ RSpec.describe BetaGouvBot::SortingHat do
 
     it 'sorts into two empty arrays' do
       expect(sorted).to eq({members: [], alumni: []})
+    end
+  end
+
+  context 'when member list has both kinds' do
+    let(:ann)		{ {id: 'ann', fullname: 'Ann', end: today.iso8601} }
+    let(:bob)		{ {id: 'bob', fullname: 'Bob', end: yesterday.iso8601} }
+    let(:members)   { [bob,ann] }
+
+    it 'sorts by splitting just after current date' do
+      expect(sorted).to match({members: [ann], alumni: [bob]})
     end
   end
 
