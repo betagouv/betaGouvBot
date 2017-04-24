382c48b153ae4f4a7825e99153adb98318e2425c
Laurent Bossavit <laurent.git@bossavit.com>
2017-04-24 15:57:17 +0200

    Change to new Slack-based protocol

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ ruby '2.3.0'
 gemspec
 
 group :development, :test do
+  gem 'rspec-collection_matchers'
   gem 'rubocop'
   gem 'rubocop-rspec'
 end
--- spec/betagouvbot/badgerequest_spec.rb
+++ spec/betagouvbot/badgerequest_spec.rb
@@ -3,21 +3,15 @@
 
 RSpec.describe BetaGouvBot::BadgeRequest do
   describe 'requesting badges' do
-    context 'when a member is working out of DINSIC offices' do
-      let(:ann)     { { id: 'ann',
-                        start: '2017-01-01',
-                        end: '2017-12-31',
-                        office: 'dinsic' }
-      }
+    context 'when a Slack user requests a badge' do
+      let(:ann)     { { id: 'ann', start: '2017-01-01', end: '2017-12-31' } }
       let(:bob)     { { id: 'bob', start: '2017-01-01', end: '2017-12-31' } }
       let(:authors) { [bob, ann] }
 
-      let(:redis)  { {} }
       let(:client) { instance_spy('client') }
 
       before do
         allow(described_class).to receive(:client) { client }
-        allow(described_class).to receive(:state) { redis }
       end
 
       describe 'selecting members' do
@@ -26,30 +20,17 @@ RSpec.describe BetaGouvBot::BadgeRequest do
         end
 
         it 'requests a badge for members who do not have one' do
-          described_class.(authors)
+          described_class.(authors, 'bob')
           expect(described_class).to have_received(:request_badge).once
-          expect(described_class).to have_received(:request_badge).with(ann)
+          expect(described_class).to have_received(:request_badge).with(bob)
         end
       end
 
       describe 'sending email' do
         it 'creates a mail action' do
-          actions = described_class.request_badge(ann)
+          actions = described_class.(authors, 'ann')
+          expect(actions).to have(1).items
           expect(actions).to include(a_kind_of(BetaGouvBot::MailAction))
-        end
-      end
-
-      describe 'managing state' do
-        it 'requests a badge only once' do
-          2.times { described_class.(authors).map(&:execute) }
-          expect(client).to have_received(:post).once
-        end
-
-        it 'requests a new badge if dates change' do
-          described_class.(authors).map(&:execute)
-          ann[:end] = '2018-12-31'
-          described_class.(authors).map(&:execute)
-          expect(client).to have_received(:post).twice
         end
       end
     end
