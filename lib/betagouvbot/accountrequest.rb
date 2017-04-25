be5d7d61178e02dfaa91805592cc0e3f18c616a7
Laurent Bossavit <laurent.git@bossavit.com>
2017-04-25 21:27:38 +0200

    Actually create accounts

--- lib/betagouvbot/accountrequest.rb
+++ lib/betagouvbot/accountrequest.rb
@@ -5,9 +5,27 @@ module BetaGouvBot
   class AccountAction
     attr_accessor :name
     attr_accessor :redirect
-    def initialize(name, redirect)
+
+    def initialize(name, redirect, password)
       @name = name
       @redirect = redirect
+      @password = password
+    end
+
+    def execute
+      accounts = api
+      endpoint = '/email/domain/beta.gouv.fr/account'
+      existing = accounts.get(endpoint, accountName: @name)
+      return if existing.length >= 1
+      accounts.post(endpoint, accountName: @name, password: @password)
+    end
+
+    def ovh
+      OVH::REST
+    end
+
+    def api
+      ovh.new(ENV['apiKey'], ENV['appSecret'], ENV['consumerKey'])
     end
   end
   module AccountRequest
@@ -15,16 +33,16 @@ module BetaGouvBot
 
     class << self
       def call(members, command)
-        member, redirect = command.split(' ')
+        member, redirect, password = command.split(' ')
         members
           .select { |author| author[:id] == member }
-          .flat_map { |author| request_account(author, redirect) }
+          .flat_map { |author| request_account(author, redirect, password) }
       end
 
-      def request_account(member, redirect)
+      def request_account(member, redirect, password)
         mail = Mail.from_file('data/mail_compte.md', [redirect])
         notify = MailAction.new(client, mail.format('author' => member))
-        perform = AccountAction.new(member[:id], redirect)
+        perform = AccountAction.new(member[:id], redirect, password)
         [perform, notify]
       end
 
---
+++ spec/betagouvbot/accountaction_spec.rb
@@ -0,0 +1,41 @@
+# encoding: utf-8
+# frozen_string_literal: true
+
+describe BetaGouvBot::AccountAction do
+  describe 'executing actions' do
+    let(:ovh)       { instance_spy('ovh') }
+    let(:api)       { instance_spy('api') }
+    let(:action)    { described_class.new('ann', 'ann@gmail.com', 'pwd') }
+    let(:endpoint)  { '/email/domain/beta.gouv.fr/account' }
+
+    before do
+      allow(action).to receive(:ovh) { ovh }
+      allow(ovh).to receive(:new) { api }
+      allow(api).to receive(:get) { [] }
+    end
+
+    it 'checks for existence of the account' do
+      action.execute
+      expect(api).to have_received(:get).with(endpoint, accountName: 'ann')
+    end
+
+    context 'when an account of that name exists' do
+      before do
+        allow(api).to receive(:get) { ['ann'] }
+      end
+      it 'checks for existence of the account' do
+        action.execute
+        expect(api).to have_received(:get).with(endpoint, accountName: 'ann')
+      end
+    end
+
+    context 'when no account of that name exists' do
+      it 'creates the account with initial password' do
+        action.execute
+        expect(api).to have_received(:get).with(endpoint, accountName: 'ann')
+        expect(api).to have_received(:post)
+          .with(endpoint, accountName: 'ann', password: 'pwd')
+      end
+    end
+  end
+end
