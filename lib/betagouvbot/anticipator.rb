02c41a12b8c88cf335b8319ff86a66976d28a58c
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2017-04-27 17:09:44 +0200

    Document Anticipator

--- lib/betagouvbot/anticipator.rb
+++ lib/betagouvbot/anticipator.rb
@@ -2,6 +2,22 @@
 # frozen_string_literal: true
 
 module BetaGouvBot
+  # Anticipator allows to detect, from a list of members (people), those whose contracts
+  # come to and end in +terms+ days.
+  #
+  # @param [<#[]>] members A collection of authors.
+  # @option members [String] :fullname An author's fullname.
+  # @option members [String] :end An author's contact end date (yyyy/mm/dd).
+  # @param [#flat_map<Integer>] terms A collection of number of days in the future.
+  # @param [#+, #iso8601] date A date to be considered as a reference for comparison.
+  #
+  # @example
+  #
+  #   Anticipator.([{ fullname: 'Laure', end: '2017-05-07' }], [10], '2017-04-27'.to_date)
+  #   #=> [{ term: 10, who: { fullname: 'Laure', end: '2017-05-07' } }]
+  #
+  # @return [<{term, who => Integer, #[]}>] A list of detected due dates grouped by
+  # +terms+.
   module Anticipator
     module_function
 
