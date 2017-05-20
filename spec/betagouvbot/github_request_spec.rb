7e647213513ecdb51e37931c37fc4bbc010cc33b
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2017-05-20 20:55:21 +0200

    Shorten test examples

--- spec/betagouvbot/github_request_spec.rb
+++ spec/betagouvbot/github_request_spec.rb
@@ -2,21 +2,20 @@
 # frozen_string_literal: true
 
 RSpec.describe BetaGouvBot::GithubRequest do
-  let(:yesterday)     { Date.today - 1 }
-  let(:tomorrow)      { Date.today + 1 }
+  subject(:actions) { described_class.(members, Date.today) }
 
-  let(:ann)     { { id: 'ann', github: 'ann-gh', end: yesterday.iso8601 } }
-  let(:bob)     { { id: 'bob', github: 'bob-gh', end: tomorrow.iso8601 } }
-  let(:ted)     { { id: 'ted', fullname: 'Ted', end: tomorrow.iso8601 } }
-  let(:members) { [bob, ann, ted] }
+  let(:yesterday)   { Date.today - 1 }
+  let(:tomorrow)    { Date.today + 1 }
+
+  let(:ann)         { { id: 'ann', github: 'ann-gh', end: yesterday.iso8601 } }
+  let(:bob)         { { id: 'bob', github: 'bob-gh', end: tomorrow.iso8601 } }
+  let(:ted)         { { id: 'ted', fullname: 'Ted', end: tomorrow.iso8601 } }
+  let(:members)     { [bob, ann, ted] }
 
   it 'ensures all active members are invited to Github org SGMAP' do
-    actions = described_class.(members, Date.today)
-    expect(actions).to have(1).items
-    matching = be_a_kind_of(BetaGouvBot::OrganizationAction)
-               .and have_attributes(user: 'bob-gh')
-               .and have_attributes(org: 'sgmap')
-               .and have_attributes(team: '2348627')
-    expect(actions).to include(matching)
+    is_expected.to have(1).items.and include(
+      be_a_kind_of(BetaGouvBot::OrganizationAction)
+        .and(have_attributes(user: 'bob-gh', org: 'sgmap', team: '2348627'))
+    )
   end
 end
