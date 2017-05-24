15df8498e4162fd6a46e086c1b76e5ec3eb27dda
Laurent Bossavit <laurent.git@bossavit.com>
2017-05-24 14:51:38 +0200

    Ensure ordering of required files

--- lib/betagouvbot.rb
+++ lib/betagouvbot.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 # frozen_string_literal: true
 
-Dir[__dir__ + '/betagouvbot/**/*.rb'].each(&method(:require))
+Dir[__dir__ + '/betagouvbot/**/*.rb'].sort.each(&method(:require))
 
 module BetaGouvBot
   #
