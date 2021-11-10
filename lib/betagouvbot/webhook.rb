6575056701aab9c21cced3844d22d7ee164cfe81
Julien DAUPHANT <1238254+jdauphant@users.noreply.github.com>
2021-11-10 21:21:58 +0000

    Retrait des jobs dépréciés

--- lib/betagouvbot/webhook.rb
+++ lib/betagouvbot/webhook.rb
@@ -28,76 +28,18 @@ module BetaGouvBot
       date = params.key?('date') ? Date.iso8601(params['date']) : Date.today
       execute = params.key?('secret') && (params['secret'] == ENV['SECRET'])
 
-      # Send contract expiration reminders (if any)
-      notifications = NotificationRequest.(members, date)
-
       # Reconcile mailing lists
       sorting_hat = SortingHat.(members, date)
 
-      # Manage Github membership
-      github = GithubRequest.(members, date)
-
       # Execute actions
-      all_actions = (notifications + sorting_hat + github)
+      all_actions = (sorting_hat)
       puts all_actions.map(&:execute) if execute
 
       # Debug
       {
         "execute": execute,
-        "notifications": notifications,
-        "sorting_hat": sorting_hat,
-        "github": github
+        "sorting_hat": sorting_hat
       }.to_json
-    end
-
-    post '/badge' do
-      badges  = BadgeRequest.(members, params['text'])
-      execute = params.key?('token') && (params['token'] == ENV['BADGE_TOKEN'])
-      badges.map(&:execute) if execute
-      { response_type: 'in_channel', text: 'OK, demande faite !' }.to_json
-    end
-
-    # Debug
-    get '/badge' do
-      { "badges": BadgeRequest.(members, params['text']) }.to_json
-    end
-
-    post '/compte' do
-      member, email, password = params['text'].to_s.split
-      account_request         = AccountRequest.new(members, member, email, password)
-
-      account_request.on(:success) do |accounts|
-        origin   = params['user_name']
-        response = "A la demande de @#{origin} je créée un compte pour #{member}"
-        respond_in_channel(response, params['response_url'])
-
-        execute = params.key?('token') && (params['token'] == ENV['COMPTE_TOKEN'])
-        begin
-          accounts.map(&:execute) if execute
-          respond('OK, création de compte en cours !', params['response_url'])
-        rescue StandardError => e
-          respond("Zut, il y a une erreur: #{e.message}", params['response_url'])
-        end
-      end
-
-      account_request.on(:not_found) do
-        respond('Je ne vois pas de qui tu veux parler', params['response_url'])
-      end
-
-      account_request.on(:error) do |errors|
-        respond(errors.first, params['response_url'])
-        raise(StandardError, errors.first)
-      end
-
-      account_request.()
-
-      # Explicitly return empty response to suppress echoing of the command
-      ''
-    end
-
-    # Debug
-    get '/compte' do
-      { "comptes": AccountRequest.(members, *params['text'].to_s.split) }.to_json
     end
 
     ## Noop
